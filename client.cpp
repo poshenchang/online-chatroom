8f47d122b3913fb6b1c8df5b901d00ad0865a30a
Ayden Chang <poshen.chang@gmail.com>
2024-12-09 20:16:25 +0800

    direct mode message

--- client.cpp
+++ client.cpp
@@ -1,10 +1,6 @@
-// TODO: Server can receive messages from the client.
-// TODO: Server can respond to client messages.
-// TODO: Client can send messages to the server.
-// TODO: Client can receive responses from the server.
-// TODO: User Registration
-// TODO: User Login
-// TODO: User Logout
+// TODO: Message encryption with OpenSSL
+// TODO: File tranfer
+// TODO: Audio streaming
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,10 +10,14 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
+#include <cstdio>
 #include <cstring>
 #include <cctype>
 #include <csignal>
 #include <ctime>
+#include <climits>
+#include <atomic>
 using namespace std;
 
 #define BUFFERSIZE 1024
@@ -44,6 +44,7 @@ const string help_msg = "------------------------------------\n"
                         "login              login authentication\n"
                         "logout             user logout\n"
                         "hello/hi           say hello!\n"
+                        "message/msg        chat with other users\n"
                         "exit               exit server session\n"
                         "------------------------------------\n";
 const string working_msg = "Utility under construction.";
@@ -52,6 +53,18 @@ const string exit_msg = "Session ended.\n"
 
 // file descriptor for communicating with server
 int socket_fd;
+FILE* server_fp;
+int listen_fd;
+
+// SIGTSTP flag
+
+atomic<bool> interrupted(false);
+
+void signalHandler(int signo){
+    if(signo == SIGTSTP){
+        interrupted.store(true);
+    }
+}
 
 // Initialize socket and connect to server
 static int connectServer(const char* serverIP, const unsigned short portnum){
@@ -84,6 +97,163 @@ static int connectServer(const char* serverIP, const unsigned short portnum){
     cout << "Connected to server at " << serverIP << ":" << portnum << endl;
 
     return 0;
+}
+
+static int setupListen(const unsigned short portnum){
+    struct sockaddr_in clientAddr;
+    int tmp;
+
+    char namebuf[512];
+    gethostname(namebuf, sizeof(namebuf));
+
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listen_fd < 0){
+        cerr << "Error creating socket" << endl;
+        return -1;
+    }
+
+    memset(&clientAddr, 0, sizeof(clientAddr));
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    clientAddr.sin_port = htons(portnum);
+    tmp = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (void*)&tmp, sizeof(tmp));
+    bind(listen_fd, (struct sockaddr*)&clientAddr, sizeof(clientAddr));
+    listen(listen_fd, 1024);
+
+    // tell server the listening port
+    sendString(socket_fd, to_string(portnum));
+
+    cerr << "listening on port "<< portnum << endl;
+    return 0;
+}
+
+// direct modes messaging
+
+pthread_t tid_send, tid_recv;
+
+struct msg_struct{
+    int socket_fd;
+    string username;
+};
+
+void* sendMessage(void* args){
+    struct msg_struct* msg_args = (struct msg_struct*)args;
+    int socket_fd = msg_args->socket_fd;
+    string username = msg_args->username;
+
+    string message;
+    while (true) {
+        cout << "> ";
+        getline(cin, message);
+        // exit when receive SIGTSTP
+        if(interrupted.load()){
+            cout << "\nCtrl+Z pressed. Leaving chatroom..." << endl;
+            interrupted.store(false);
+            break;
+        }
+        if (send(socket_fd, message.c_str(), message.size(), 0) <= 0) break;
+    }
+    pthread_cancel(tid_recv);
+    return nullptr;
+}
+
+void* receiveMessage(void* args){
+    struct msg_struct* msg_args = (struct msg_struct*)args;
+    int socket_fd = msg_args->socket_fd;
+    string username = msg_args->username;
+
+    char buf[1024];
+    while(true){
+        ssize_t nbytes = recv(socket_fd, buf, sizeof(buf)-1, 0);
+        // exit when remote disconnects
+        if(nbytes <= 0){
+            cout << "User " << username << " has left." << endl;
+            break;
+        }
+        buf[nbytes] = '\0';
+        cout << '\r' << username << ": " << buf << "\n> " << flush; // Redisplay prompt
+    }
+    pthread_cancel(tid_send);
+    return nullptr;
+}
+
+static int directMessage(string username, bool init, string remoteHost, string remotePort){
+    int msg_fd;
+    if(init){
+        struct sockaddr_in cliAddr;
+        unsigned short portnum = stoi(remotePort);
+        // Create the socket
+        msg_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if(msg_fd < 0){
+            cerr << "Error creating socket" << endl;
+            return -1;
+        }
+        // Set up the server address structure
+        memset(&cliAddr, 0, sizeof(cliAddr));
+        cliAddr.sin_family = AF_INET;
+        cliAddr.sin_port = htons(portnum);
+        if(inet_pton(AF_INET, remoteHost.c_str(), &cliAddr.sin_addr) <= 0){
+            cerr << remoteHost << endl;
+            cerr << "Invalid Address" << endl;
+            close(msg_fd);
+            return -1;
+        }
+        // Connect to the server
+        if(connect(msg_fd, (struct sockaddr*)&cliAddr, sizeof(cliAddr)) < 0){
+            cerr << "Connection Failed" << endl;
+            close(msg_fd);
+            return -1;
+        }
+    }
+    else{
+        struct sockaddr_in cliAddr;
+        size_t clilen;
+
+        clilen = sizeof(cliAddr);
+        msg_fd = accept(listen_fd, (struct sockaddr*)&cliAddr, (socklen_t*)&clilen);
+        if(msg_fd < 0){
+            if(errno == EINTR || errno == EAGAIN) return -1;  // try again
+            cerr << "Cannot accept client connection" << endl;
+        }
+    }
+
+    // clear cin buffer
+    string _str; getline(cin, _str);
+    cout << "Established connection with " << username << "." << endl;
+    cout << "Press Ctrl+Z to exit the chatroom." << endl;
+    cout << "Enjoy your time!\n"
+            "------------------------------------" << endl;
+    
+    struct msg_struct *args =(struct msg_struct*)malloc(sizeof(struct msg_struct));
+    args->socket_fd = msg_fd;
+    args->username = username;
+    pthread_create(&tid_send, NULL, sendMessage, args);
+    pthread_create(&tid_recv, NULL, receiveMessage, args);
+    
+    // cleanup
+    pthread_join(tid_recv, NULL);
+    close(msg_fd);
+    pthread_join(tid_send, NULL);
+    cout << "------------------------------------" << endl;
+    return 0;
+}
+
+// write std::string to file pointer
+int sendLine(FILE* fp, string msg){
+    return fprintf(fp, "%s\n", msg.c_str());
+}
+
+// read std::string from file pointer
+int receiveLine(FILE* fp, string &msg){
+    char* buf = NULL; size_t len;
+    int nbytes = getline(&buf, &len, fp);
+    if(nbytes > 0){
+        msg = buf;
+        if(msg[nbytes-1] == '\n') msg.pop_back();
+    }
+    free(buf);
+    return nbytes;
 }
 
 // write std::string to file descriptor
@@ -146,22 +316,46 @@ void strToLower(string& str){
 }
 
 int main(int argc, char** argv){
-    if(argc != 3){
-        cerr << "Usage: " << argv[0] << " [IP address] [port]" << endl;
+    if(argc != 4){
+        cerr << "Usage: " << argv[0] << " [server IP address] [server port] [client port]" << endl;
         exit(1);
     }
 
     // Initialize socket and connect to server
     connectServer(argv[1], strToInt(argv[2]));
+    setupListen(strToInt(argv[3]));
+    server_fp = fdopen(socket_fd, "a+");
+    setbuf(server_fp, NULL);
+
+    // Initialize signal handler
+    struct sigaction sa;
+    sa.sa_handler = signalHandler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGTSTP, &sa, NULL);
 
     cout << welcome_msg;
     bool exit = false;
-    // 'cmd' for reading command from client; 'status' for reading return status from server
+    // 'cmd' for reading command from client; 'stat' for reading return status from server
     string cmd, stat;
     string currentUser = "";
     while(!exit){
+        // check for pending message requests
+        if(!currentUser.empty()){
+            sendLine(server_fp, "msgNum");
+            string buf;
+            receiveLine(server_fp, buf);
+            int numReq = stoi(buf);
+            if(numReq > 0){
+                cout << "[Notification] You have " << numReq << " pending message requests. Type 'message' to enter chatroom.\n";
+            }
+        }
+
         cout << cmd_prompt;
-        cin >> cmd;
+        while(!(cin >> cmd)){
+            cin.clear();
+            // cin.ignore(INT_MAX, '\n');
+        }
         strToLower(cmd);
         if(cmd == "help"){
             cout << help_msg;
@@ -204,7 +398,7 @@ int main(int argc, char** argv){
             cin >> username;
             sendString(socket_fd, "checkuser " + username + '\n');
             if(!isSuccess()){
-                cout << "Username does not exist." << endl;
+                cout << "User does not exist." << endl;
                 continue;
             }
             // Enter password
@@ -218,6 +412,7 @@ int main(int argc, char** argv){
             else cout << "Login failed. Wrong password or user already logged in at another session." << endl;
         }
         else if(cmd == "logout"){
+            // Check if logged in
             if(currentUser.empty()){
                 cout << "You are not currently logged in. Please log in first." << endl;
                 continue;
@@ -228,6 +423,66 @@ int main(int argc, char** argv){
                 currentUser = "";
             }
             else cout << "Logout failed." << endl;
+        }
+        else if(cmd == "message" || "msg"){
+            // Check if logged in
+            if(currentUser.empty()){
+                cout << "You are not currently logged in. Please log in first." << endl;
+                continue;
+            }
+            // get pending message requests
+            sendLine(server_fp, "msgReq");
+            set<string> reqSet;
+            string buf;
+            receiveLine(server_fp, buf);
+            int numReq = stoi(buf);
+            if(numReq > 0){
+                cout << "Pending message requests from:" << endl;
+                for(int i=0; i<numReq; i++){
+                    receiveLine(server_fp, buf);
+                    reqSet.insert(buf);
+                    cout << "* " << buf << endl;
+                }
+            }
+
+            string username;
+            cout << "Who do you want to message? Enter username: ";
+            cin >> username;
+            // User cannot message themselves
+            if(username == currentUser){
+                cout << "Choose a user other than yourself to message." << endl;
+                continue;
+            }
+            // Check if user is in pending requests
+            if(reqSet.count(username)){
+                // accept message
+                sendLine(server_fp, "accept " + username);
+                if(!isSuccess()){
+                    cout << "Message request from " << username << " expired." << endl;
+                }
+                string remoteHost, remotePort;
+                directMessage(username, false, remoteHost, remotePort);
+            }
+            else{
+                // Check if user exists
+                sendString(socket_fd, "checkuser " + username + '\n');
+                if(!isSuccess()){
+                    cout << "User does not exist." << endl;
+                    continue;
+                }
+                // Send message request
+                sendString(socket_fd, "connect " + username + '\n');
+                cout << "Connecting to " << username << ", request pending..." << endl;
+                if(!isSuccess()){
+                    cout << "Request failed. User offline or request timeout." << endl;
+                    continue;
+                }
+                // get client IP and portnum
+                string remoteHost, remotePort;
+                receiveLine(server_fp, remoteHost);
+                receiveLine(server_fp, remotePort);
+                directMessage(username, true, remoteHost, remotePort);
+            }            
         }
         else if(cmd == "hello" || cmd == "hi"){
             if(!currentUser.empty()){
--- server.cpp
+++ server.cpp
@@ -1,10 +1,6 @@
-// TODO: Server can receive messages from the client.
-// TODO: Server can respond to client messages.
-// TODO: Client can send messages to the server.
-// TODO: Client can receive responses from the server.
-// TODO: User Registration
-// TODO: User Login
-// TODO: User Logout
+// TODO: Message encryption with OpenSSL
+// TODO: File tranfer
+// TODO: Audio streaming
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,6 +13,8 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <set>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <cctype>
@@ -27,6 +25,7 @@ using namespace std;
 #define BUFFERSIZE 1024
 #define MAXCMDARG 3
 #define MAXWORKERS 10
+#define MAXWAITTIME 60
 
 // Function prototypes
 
@@ -44,36 +43,87 @@ inline void sendFail(int socket_fd){
     sendString(socket_fd, "FAIL\n");
 }
 
+// maximum number of file descriptors
+int maxfd;
+// server hostname
+string hostname;
+// file descriptor for listening incoming requesets
+int listen_fd;
+// server port
+unsigned short port;
+
 // Client
 
 class Client{
     public:
         string host;                // client's host
         int connect_fd;             // fd to talk with client
+        string listen_port;         // listening port of client
         string username;            // username of client connection
-        string buf;                 // data sent by/to client
-        size_t buf_len;             // bytes used by buf
         bool login;
         bool alive;
 
         Client(){}
-        Client(string host, int fd);
+        Client(string host, int fd, string portnum);
 };
 
-Client::Client(string host, int fd){
+Client::Client(string host, int fd, string portnum){
     this->host = host;
     this->connect_fd = fd;
+    this->listen_port = portnum;
     this->username.clear();
-    this->buf_len = 0;
     this->login = false;
     this->alive = true;
 }
+
+// stores data of ongoing clients
+map<int, Client> req;
+pthread_rwlock_t reqRWLock = PTHREAD_RWLOCK_INITIALIZER;
 
 enum status{
     ONLINE,
     OFFLINE,
     IDLE
 };
+
+class threadSignal{
+    private:
+        bool state;
+        pthread_cond_t sigCond;
+        pthread_mutex_t sigMutex;
+    public:
+        threadSignal();
+        int wait(int sec);
+        void signal();
+};
+
+threadSignal::threadSignal(){
+    state = false;
+    sigCond = PTHREAD_COND_INITIALIZER;
+    sigMutex = PTHREAD_MUTEX_INITIALIZER;
+}
+
+int threadSignal::wait(int sec){
+    pthread_mutex_lock(&sigMutex);
+    if(!state){
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += sec;
+        pthread_cond_timedwait(&sigCond, &sigMutex, &ts);
+    }
+    bool saveState = state;
+    state = false;
+    pthread_mutex_unlock(&sigMutex);
+    if(!saveState) return -1;
+    return 0;
+}
+
+void threadSignal::signal(){
+    pthread_mutex_lock(&sigMutex);
+    state = true;
+    pthread_cond_signal(&sigCond);
+    pthread_mutex_unlock(&sigMutex);
+}
 
 // User
 
@@ -82,6 +132,9 @@ class User{
         string username;
         string password;
         status stat;
+        int connect_fd;
+        set<string> reqSet;
+        threadSignal acceptReq;
 
         User(){}
         User(string username, string password);
@@ -96,50 +149,109 @@ User::User(string username, string password){
 class UserDB{
     private:
         map<string, User> reg;
-        map<string, vector<string> > msg;
+        pthread_rwlock_t userRWLock;
     public:
+        UserDB(){
+            pthread_rwlock_t userRWLock = PTHREAD_RWLOCK_INITIALIZER;
+        };
         bool checkUser(string username);
+        bool checkOnline(string username);
         void userRegister(string username, string password);
-        bool userLogin(string username, string password);
+        bool userLogin(string username, string password, int connect_fd);
         void userLogout(string username);
+        int msgRequest(string from, string to);
+        int acceptRequest(string from, string to);
+        int requestCount(string username);
+        set<string> getRequest(string username);
+        int getHostPort(string username, string& host, string& portnum);
 };
 
 bool UserDB::checkUser(string username){
-    return (reg.count(username));
+    pthread_rwlock_rdlock(&userRWLock);
+    bool retval = reg.count(username);
+    pthread_rwlock_unlock(&userRWLock);
+    return retval;
+}
+
+bool UserDB::checkOnline(string username){
+    pthread_rwlock_rdlock(&userRWLock);
+    bool retval =  (reg.count(username) && reg[username].stat == ONLINE);
+    pthread_rwlock_unlock(&userRWLock);
+    return retval;
 }
 
 void UserDB::userRegister(string username, string password){
+    pthread_rwlock_wrlock(&userRWLock);
     reg.insert(make_pair(username, User(username, password)));
-    msg.insert(make_pair(username, vector<string>()));
+    pthread_rwlock_unlock(&userRWLock);
 }
 
-bool UserDB::userLogin(string username, string password){
+bool UserDB::userLogin(string username, string password, int connect_fd){
     cerr << "check password " << password << endl;
+    pthread_rwlock_wrlock(&userRWLock);
     if(reg[username].password == password && reg[username].stat == OFFLINE){
         reg[username].stat = ONLINE;
+        reg[username].connect_fd = connect_fd;
+        pthread_rwlock_unlock(&userRWLock);
         return true;
     }
+    pthread_rwlock_unlock(&userRWLock);
     return false;
 }
 
 void UserDB::userLogout(string username){
+    pthread_rwlock_wrlock(&userRWLock);
     reg[username].stat = OFFLINE;
+    pthread_rwlock_unlock(&userRWLock);
 }
 
-// maximum number of file descriptors
-int maxfd;
-// server hostname
-string hostname;
-// file descriptor for listening incoming requesets
-int listen_fd;
-// server port
-unsigned short port;
-// stores data of ongoing clients
-map<int, Client> req;
-pthread_mutex_t reqMutex = PTHREAD_MUTEX_INITIALIZER;
+int UserDB::msgRequest(string from, string to){
+    pthread_rwlock_wrlock(&userRWLock);
+    reg[to].reqSet.insert(from);
+    pthread_rwlock_unlock(&userRWLock);
+    int retval = reg[from].acceptReq.wait(MAXWAITTIME);
+    reg[to].reqSet.erase(from);
+    return retval;
+}
+
+int UserDB::acceptRequest(string from, string to){
+    pthread_rwlock_rdlock(&userRWLock);
+    if(!reg[to].reqSet.count(from)){
+        pthread_rwlock_unlock(&userRWLock);
+        return -1;
+    }
+    pthread_rwlock_unlock(&userRWLock);
+    reg[from].acceptReq.signal();
+    return 0;
+}
+
+int UserDB::requestCount(string username){
+    pthread_rwlock_rdlock(&userRWLock);
+    int size =  reg[username].reqSet.size();
+    pthread_rwlock_unlock(&userRWLock);
+    return size;
+}
+set<string> UserDB::getRequest(string username){
+    pthread_rwlock_rdlock(&userRWLock);
+    set<string> Set =  reg[username].reqSet;
+    pthread_rwlock_unlock(&userRWLock);
+    return Set;
+}
+
+int UserDB::getHostPort(string username, string& host, string& portnum){
+    pthread_rwlock_rdlock(&userRWLock);
+    if(!reg.count(username)){
+        pthread_rwlock_unlock(&userRWLock);
+        return -1;
+    }
+    host = req[reg[username].connect_fd].host;
+    portnum = req[reg[username].connect_fd].listen_port;
+    pthread_rwlock_unlock(&userRWLock);
+    return 0;
+}
+
 // stores data of registered users
 UserDB userDatabase;
-pthread_mutex_t userMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void signalHandler(int signo) {
     if(signo == SIGPIPE){
@@ -195,18 +307,28 @@ int acceptConnection(){
         cerr << "Cannot accept client connection" << endl;
     }
 
-    // TODO: initialize new Client
+    // initialize new Client
     string client_host = inet_ntoa(cliaddr.sin_addr);
-    req.insert(make_pair(connect_fd, Client(client_host, connect_fd)));
-    cout << "new client from " << client_host << '\n';
+    string portnum = receiveString(connect_fd);
+    pthread_rwlock_wrlock(&reqRWLock);
+    req.insert(make_pair(connect_fd, Client(client_host, connect_fd, portnum)));
+    pthread_rwlock_unlock(&reqRWLock);
+    cout << "New client from " << client_host << ", client listening on " << portnum << endl;
     return connect_fd;
 }
 
 int closeConnection(int fd){
+    pthread_rwlock_rdlock(&reqRWLock);
     Client cli = req[fd];
+    pthread_rwlock_unlock(&reqRWLock);
+
     close(cli.connect_fd);
     cerr << "closed connection with " << cli.host << endl;
+
+    pthread_rwlock_wrlock(&reqRWLock);
     req.erase(fd);
+    pthread_rwlock_unlock(&reqRWLock);
+
     return 0;
 }
 
@@ -216,7 +338,9 @@ void sendString(int socket_fd, string msg){
     if(nbytes == -1){
         cerr << "Failed to send message" << endl;
         if(errno == EPIPE){
+            pthread_rwlock_wrlock(&reqRWLock);
             req[socket_fd].alive = false;
+            pthread_rwlock_unlock(&reqRWLock);
             cerr << "Client has disconnected" << endl;
         }
     }
@@ -232,7 +356,9 @@ string receiveString(int socket_fd){
     }
     else if(nbytes == 0){
         cerr << "Client terminated prematurely" << endl;
+        pthread_rwlock_wrlock(&reqRWLock);
         req[socket_fd].alive = false;
+        pthread_rwlock_unlock(&reqRWLock);
     }
     else if(nbytes > 0){
         str.append(buf, nbytes);
@@ -281,9 +407,8 @@ void* worker_thread(void* arg){
         cout << "Worker " << pthread_self() << " handling client " << task.client_fd << endl;
 
         bool exit = false;
+        string currentUser = "";
         while(!exit){
-            string currentUser = "";
-
             string cmdstr = receiveString(connect_fd);
             cerr << "[command] " << cmdstr << endl;
             stringstream cmdarg(cmdstr);
@@ -305,7 +430,7 @@ void* worker_thread(void* arg){
             else if(cmd == "login"){
                 string username, password;
                 cmdarg >> username >> password;
-                if(userDatabase.userLogin(username, password)){
+                if(userDatabase.userLogin(username, password, connect_fd)){
                     currentUser = username;
                     sendSuccess(connect_fd);
                 }
@@ -313,15 +438,67 @@ void* worker_thread(void* arg){
             }
             else if(cmd == "logout"){
                 userDatabase.userLogout(currentUser);
-                currentUser = "";
+                currentUser.clear();
                 sendSuccess(connect_fd);
+            }
+            else if(cmd == "connect"){
+                string username;
+                cmdarg >> username;
+                if(!userDatabase.checkOnline(username)){
+                    sendFail(connect_fd);
+                    continue;
+                }
+                if(userDatabase.msgRequest(currentUser, username) < 0){
+                    sendFail(connect_fd);
+                    continue;
+                }
+                sendSuccess(connect_fd);
+                // send client IP and portnum
+                string host, portnum;
+                userDatabase.getHostPort(username, host, portnum);
+                sendString(connect_fd, host + '\n' + portnum + '\n');
+            }
+            else if(cmd == "accept"){
+                string username;
+                cmdarg >> username;
+                if(userDatabase.acceptRequest(username, currentUser) < 0){
+                    sendFail(connect_fd);
+                    continue;
+                }
+                sendSuccess(connect_fd);
+            }
+            else if(cmd == "msgNum"){
+                if(currentUser.empty()){
+                    sendString(connect_fd, "0\n");
+                }
+                else{
+                    int reqNum = userDatabase.requestCount(currentUser);
+                    sendString(connect_fd, to_string(reqNum) + '\n');
+                }
+            }
+            else if(cmd == "msgReq"){
+                if(currentUser.empty()){
+                    sendString(connect_fd, "0\n");
+                }
+                else{
+                    set<string> reqSet = userDatabase.getRequest(currentUser);
+                    sendString(connect_fd, to_string(reqSet.size()) + '\n');
+                    for(auto it = reqSet.begin(); it != reqSet.end(); ++it){
+                        sendString(connect_fd, *it + '\n');
+                    }
+                }
             }
             else if(cmd == "exit"){
                 exit = true;
             }
 
             // Check if client is alive
+            pthread_rwlock_rdlock(&reqRWLock);
             if(!req[connect_fd].alive) exit = true;
+            pthread_rwlock_unlock(&reqRWLock);
+        }
+        if(!currentUser.empty()){
+            userDatabase.userLogout(currentUser);
         }
         closeConnection(connect_fd);
     }
